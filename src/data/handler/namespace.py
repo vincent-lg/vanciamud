a806991b78a4efeeb360b9fbf97924e784e3b5db
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2022-04-21 12:53:01 +0200

    Implement lazy loading of namespaces to prevent recursion

--- src/data/handler/namespace.py
+++ src/data/handler/namespace.py
@@ -30,6 +30,7 @@
 """Namespace custom field."""
 
 import pickle
+from typing import Any
 
 from pygasus.model import CustomField
 
@@ -43,47 +44,83 @@ class Namespace(dict):
     def __init__(self, *args, **kwargs):
         self.parent = None
         self.field = None
+        self._bytes = None
         super().__init__(*args, **kwargs)
 
     def __getattr__(self, key):
-        if key in ("parent", "field"):
+        if key in ("parent", "field", "_bytes"):
             return object.__getattr__(self, key)
 
+        self._load()
         value = super().__getitem__(key)
         return value
 
     def __setattr__(self, key, value):
-        if key in ("parent", "field"):
+        if key in ("parent", "field", "_bytes"):
             object.__setattr__(self, key, value)
         else:
+            self._load()
             super().__setitem__(key, value)
             self.save()
 
     def __delattr__(self, key):
-        if key in ("parent", "field"):
+        if key in ("parent", "field", "_bytes"):
             object.__delattr__(self, key)
         else:
+            self._load()
             super().__delitem__(key)
             self.save()
 
+    def __contains__(self, element: Any):
+        self._load()
+        return super().__contains__(element)
+
+    def __repr__(self):
+        self._load()
+        return super().__repr__()
+
+    def __str__(self):
+        self._load()
+        return super().__str__()
+
     def __getitem__(self, key):
+        self._load()
         value = super().__getitem__(key)
         return value
 
     def __setitem__(self, key, value):
+        self._load()
         super().__setitem__(key, value)
         self.save()
 
     def __delitem__(self, key):
+        self._load()
         super().__delitem__(key)
         self.save()
 
     def clear(self):
+        self._load()
         if self:
             super().clear()
             self.save()
 
+    def get(self, key: str, value: Any = _NOT_SET):
+        self._load()
+        if value is _NOT_SET:
+            return super().get(key)
+
+        return super().get(key)
+
+    def items(self):
+        self._load()
+        return super().items()
+
+    def keys(self):
+        self._load()
+        return super().keys()
+
     def pop(self, key, default=_NOT_SET):
+        self._load()
         if default is _NOT_SET:
             value = super().pop(key)
         else:
@@ -92,21 +129,36 @@ class Namespace(dict):
         return value
 
     def popitem(self):
+        self._load()
         pair = super().popitem()
         self.save()
         return pair
 
     def setdefault(self, key, default=None):
+        self._load()
         value = super().setdefault(key, default)
         self.save()
         return value
 
     def update(self, *args, **kwargs):
+        self._load()
         super().update(*args, **kwargs)
         self.save()
 
+    def values(self):
+        self._load()
+        return super().values()
+
+    def _load(self):
+        """Load, if necessary, the dictionary."""
+        if self._bytes is not None:
+            # Actually load the dictionary.
+            super().update(pickle.loads(self._bytes))
+            self._bytes = None
+
     def save(self):
         """Save the dictionary into the parent."""
+        self._load()
         type(self.parent).repository.update(
             self.parent, self.field, {}, self.copy()
         )
@@ -152,4 +204,6 @@ class NamespaceField(CustomField):
             in the model.
 
         """
-        return Namespace(pickle.loads(value))
+        namespace = Namespace()
+        namespace._bytes = value
+        return namespace
